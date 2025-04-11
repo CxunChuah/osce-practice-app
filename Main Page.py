ba4940f953f7f3ef661e150ce9268dd5a5a72b19
CxunChuah <cxunchuah@hotmail.com>
2025-04-11 23:57:52 +0800

    Update Main Page.py

--- Main Page.py
+++ Main Page.py
@@ -7,7 +7,7 @@ from sendgrid.helpers.mail import Mail
 # 📬 Email-sending function (HTML version)
 def send_verification_email(to_email, code):
     message = Mail(
-        from_email='your_verified_sender@example.com',  # replace with your verified sender
+        from_email='bphf21106118@mahsastudent.edu.my',  # replace with your verified sender
         to_emails=to_email,
         subject='Your OSCE App Verification Code',
         html_content=f"""
