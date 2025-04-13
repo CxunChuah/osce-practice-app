3da4bc554e6e36ab944fb5e199cc29d55f5601ee
CxunChuah <cxunchuah@hotmail.com>
2025-04-13 18:07:34 +0800

    Update __init__.py

--- stations/msk/__init__.py
+++ stations/msk/__init__.py
@@ -1,1 +1,4 @@
-skibi
+# Makes the msk module a package
+# Import station display functions
+from stations.msk.shoulder import display_station as display_shoulder_station
+from stations.msk.knee import display_station as display_knee_station
